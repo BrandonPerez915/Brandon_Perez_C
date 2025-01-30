6c3419c47272136a9a3692cd87c0525b1364b5b6
Brandon Perez <brandon.perez@iteso.mx>
2025-01-30 09:12:21 -0600

    Alan Bot v1.0 (datos_usuario)

--- alan_bot.c
+++ alan_bot.c
@@ -3,15 +3,20 @@
 int main() {
   int i_edad;
   float f_estatura;
-  char c_nombre, c_estado_civil;
+  char c_nombre[50], c_estado_civil;
 
   printf("Hola, soy AlanBot?\n");
   printf("Ingrese su nombre:\n");
-  scanf("%s", &c_nombre);
+  scanf("%s", c_nombre);
   printf("Ingrese su estatura:\n");
   scanf("%f", &f_estatura);
   printf("Ingrese su edad:\n");
   scanf("%d", &i_edad);
+  getchar();
   printf("Ingrese su estado civil:\n");
   scanf(" %c", &c_estado_civil);
+
+  printf("Hola %s \n Su estatura es: %f \n Su edad es: %d \n Su estado civil es: %c \n", c_nombre, f_estatura, i_edad, c_estado_civil);
+
+  return 0;
 }
